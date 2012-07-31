f975cb1d8467799c457101f1b721f199cffe03da
Grzegorz Rolek <ichosis@gmail.com>
2012-07-31 21:03:24 +0200

    Enforce specific GLIF format on lint if necessary.

--- lint.sh
+++ lint.sh
@@ -7,8 +7,23 @@
 # Make sure 'xmllint' is available.
 type xmllint &>/dev/null || { echo >&2 "Fatal: No 'xmllint' found; can not proceed."; exit 2; }
 
+# Prepare the option index and look for format enforcement option.
+OPTIND=1; getopts "f:" OPTION "$@"
+
+# If found, pass only those that make sense; allow a zeroth (i.e., to be read from file) for consistency.
+test "$OPTION" = "f" && if test "$OPTARG" = 1 || test "$OPTARG" = 2 || test "$OPTARG" = 0
+	then FORMAT=$OPTARG
+	else echo >&2 "Fatal: Bad format version: $OPTARG; must be 0-2."; exit 2
+	fi
+
+# Skip the options and get into the file paths.
+shift $(expr $OPTIND - 1)
+
 # Make sure there's at least one path given.
-test $# = 0 && { echo >&2 "Usage: $(basename $0) UFO [UFO ...]"; exit 2; }
+test $# = 0 && { echo >&2 "Usage: $(basename $0) [-f FORMAT] UFO [UFO ...]"; exit 2; }
+
+# That's a quote of an error message to suppress on format enforcement.
+QUOTE="Element 'glyph', attribute 'format':"
 
 while test "$1" != ""
 do
@@ -24,15 +39,18 @@ do
 		for GLIF in $(cd "$UFO"; ls glyphs*/*.glif)
 		do
 
-			# Dig out .glif file's format version; assume first if not declared.
-			FORMAT=$(sed -n "/<glyph .*format=[\"']\([1-2]\)[\"'].*>/s//\1/p" <"$UFO"/$GLIF)
+			# If no format was enforced, read it from a file; assume first if not declared.
+			test ${FORMAT=0} = 0 && FORMAT=$(sed -n "/<glyph .*format=[\"']\([1-2]\)[\"'].*>/s//\1/p" <"$UFO"/$GLIF)
 			! test $FORMAT && FORMAT=1
 
 			# Validate the file against proper GLIF Schema and catch the results.
 			RESULT=$(xmllint --noout --schema "`dirname \"$0\"`"/GLIF$FORMAT/glyph.xsd "$UFO"/$GLIF 2>&1)
 
-			# Record failed validation and spit the errors, if any.
-			test $? != 0 && STATUS=1 && echo >&2 "$RESULT"
+			# Inform about failure, but only if the error does not relate to format enforcement.
+			# Note that the filter part is fragile as it depends on xmllint's literal error message.
+			if test $? != 0 && ! (echo "$RESULT" | grep --quiet "$QUOTE" && test $(wc -l <<<"$RESULT") = 2)
+			then STATUS=1; echo "$RESULT" | sed "/$QUOTE/ d" >&2
+			fi
 
 		done
 
